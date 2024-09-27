f3bef42d04b99ab0978b5d78ea53ed7523b111ec
KEYY_Tech <emmanuelyayrakotoka@gmail.com>
2024-09-27 22:32:59 +0100

    Working On the Work

--- dashboard.php
+++ dashboard.php
@@ -4,5 +4,15 @@ session_start();
 
 
 ?>
-<!DOCTYPE html>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>BookShop</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/bootstrap.bundle.js"></script>
+</head>
+<body>
+
+</body>
+</html>
